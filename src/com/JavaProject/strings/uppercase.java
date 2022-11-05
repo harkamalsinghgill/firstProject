9728b5369d9688324e7f4cfcf107a5746611fdbb
harkamalsinghgill <gkamal222@gmail.com>
2022-11-05 08:34:43 -0700

    vivek changes

--- src/com/JavaProject/strings/uppercase.java
+++ src/com/JavaProject/strings/uppercase.java
@@ -4,12 +4,9 @@ public class uppercase {
 
     public static String toUppercase( String str){
 
-
-
     }
 
     public static void main(String[] args) {
-        String str = "hi i am shradha";
         System.out.println("vivek changes");
 
 
